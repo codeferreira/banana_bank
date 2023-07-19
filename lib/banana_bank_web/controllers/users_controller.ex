5dbd23a3dd6bf37d36d16ce3ee7cf1b02127016b
José Ferreira <dev.josejunior@gmail.com>
2023-07-19 19:13:28 -0300

    feat: create fallback controller

--- lib/banana_bank_web/controllers/users_controller.ex
+++ lib/banana_bank_web/controllers/users_controller.ex
@@ -1,23 +1,16 @@
 defmodule BananaBankWeb.UsersController do
-  alias BananaBank.Users.Create
   use BananaBankWeb, :controller
 
+  alias BananaBank.Users
+  alias Users.User
+
+  action_fallback BananaBankWeb.FallbackController
+
   def create(conn, params) do
-    params
-    |> Create.call()
-    |> handle_response(conn)
-  end
-
-  def handle_response({:ok, user}, conn) do
-    conn
-    |> put_status(:created)
-    |> render(:create, user: user)
-  end
-
-  def handle_response({:error, changeset}, conn) do
-    conn
-    |> put_status(:bad_request)
-    |> put_view(json: BananaBankWeb.ErrorJSON)
-    |> render(:error, changeset: changeset)
+    with {:ok, %User{} = user} <- Users.create(params) do
+      conn
+      |> put_status(:created)
+      |> render(:create, user: user)
+    end
   end
 end
